8f85ff908e6a2bf9234c679bd929199bf1347305
Swapnil Supekar <swpnilsupekar@gmail.com>
2026-01-31 22:43:34 +0530

    loop

---
+++ Java_Learn/src/loop.java
@@ -0,0 +1,17 @@
+public class loop {
+    // Print numbers from 1 to 10 using a for loop
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+    //print numbers from 1 to 10 using while loop
+    public static void whileLoop() {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
