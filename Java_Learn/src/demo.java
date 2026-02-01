d71d00f06575a35156302ed7429b442118be605c
Swapnil Supekar <swpnilsupekar@gmail.com>
2026-02-01 20:44:47 +0530

    datatype

--- Java_Learn/src/demo.java
+++ Java_Learn/src/demo.java
@@ -1,8 +1,22 @@
 
 public class demo {
 
+
 	public static void main(String[] args) {
+		
 		System.out.println("hello");
+
+		// datatypes
+		int num=5; 
+		float f=2.4f;
+		char ch='s';
+		double d=2.3;
+		//System.out.println("The number is: " + num);
+	    System.out.println("int:"+num+" float:"+f+" Char:"+ch+" double"+d);	
+
+     
+
+
 
 	}
 
