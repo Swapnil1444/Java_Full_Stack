0ad2519bdc021bd9cc98609f8361b465d1e380d9
Swapnil Supekar <swpnilsupekar@gmail.com>
2026-02-08 22:11:18 +0530

    condition statement

---
+++ Java_Learn/src/Conditional_Statements.java
@@ -0,0 +1,55 @@
+public class Conditional_Statements {
+
+    public static void main(String[] args) {
+        int a=12;
+        if(a>=15){
+         System.out.println("value is similler to 15 "+a);
+        }
+
+
+       // if-else
+        if(true){
+
+        }
+        else{
+
+        }
+
+        //if-else-if
+        if(true) {
+
+        }
+        else  {
+
+        }   
+         if (true) {
+            
+         }   
+
+       // nested if-else  
+         if(true){
+            if(true){
+
+            }
+            else{
+
+            }
+         }else{
+
+         }
+
+         //if -else-if ladder
+         if(true){
+
+         }else if(true){
+
+         }else if(true){
+
+         }else{
+            
+         }
+
+
+
+    }
+}
