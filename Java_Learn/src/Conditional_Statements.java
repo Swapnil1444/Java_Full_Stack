2cd7d3ce995080c683945ffb3e62f46cae6536fd
Swapnil Supekar <swpnilsupekar@gmail.com>
2026-02-09 22:04:20 +0530

    loops

--- Java_Learn/src/Conditional_Statements.java
+++ Java_Learn/src/Conditional_Statements.java
@@ -1,19 +1,25 @@
 public class Conditional_Statements {
 
     public static void main(String[] args) {
-        int a=12;
-        if(a>=15){
-         System.out.println("value is similler to 15 "+a);
-        }
+        
+        // if(a>=15){
+        //  System.out.println("value is similler to 15 "+a);
+        // }
 
+        Integer a=null;
 
-       // if-else
-        if(true){
-
+       // if-else write a code to check even or odd number.
+       int num=1444;
+        if(num%2==0){
+            System.out.println("even");
         }
         else{
-
+            System.out.println("odd");
         }
+
+        //tarnary opereter to convart if -else
+            String result=(num%2==0)?"even":"odd";
+             System.out.println(result);
 
         //if-else-if
         if(true) {
@@ -46,7 +52,35 @@ public class Conditional_Statements {
          }else if(true){
 
          }else{
-            
+
+         }
+
+         //switch - write a program to check the day on week 
+         num=4;
+         switch(num){
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                 break;
+            case 3:
+                 System.out.println("Wednesday");
+                  break;
+            case 4:
+                 System.out.println("Thursday");
+                  break;
+            case 5:
+                 System.out.println("Friday");
+                  break;
+            case 6:
+                 System.out.println("Saturday");
+                  break;
+            case 7:    
+                 System.out.println("Sunday");
+                  break;
+            default:
+                System.out.println("Invalid number");
          }
 
 
--- Java_Learn/src/loop.java
+++ Java_Learn/src/loop.java
@@ -1,17 +1,45 @@
 public class loop {
-    // Print numbers from 1 to 10 using a for loop
+    
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-    }
-    //print numbers from 1 to 10 using while loop
-    public static void whileLoop() {
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i);
+     
+        //while loop -print the 4 table 
+        int num=4;
+        int i=1;
+
+        while (i<=10) {
+            System.out.println(i*num);
             i++;
+        } 
+
+       // do while loop - print the hello in one time without condition true.
+       i=20;
+        do{
+            System.out.println("hello");
+            i++;
+        }while(i<=10);
+
+        //for loop - create a calendar to day and hours on 9-16 on 5 day and show the dayly work
+
+        for(i=1;i<=5;i++){
+            System.out.println("DAY :"+i);
+            for(int j=1;j<9;j++){
+               System.out.println("     "+(j+8)+" - "+(j+9));
+            }
         }
+        // work on while loop using the for loop
+        i=1;
+        for(;i<=10;){
+           System.out.println(i);
+           i++;
+        }
+
+        // for each loop - print the array value .
+        int arr[]={10,20,30,40,50};
+        for(int value:arr){
+            System.out.print(" "+value);
+        }
+
+
     }
     
 }
