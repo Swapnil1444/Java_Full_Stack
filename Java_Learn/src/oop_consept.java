c06535dfcb95cb8f03f902d61c324a87700fb0d8
Swapnil Supekar <swpnilsupekar@gmail.com>
2026-02-10 11:28:33 +0530

    oop_consept

---
+++ Java_Learn/src/oop_consept.java
@@ -0,0 +1,20 @@
+
+// class - to crate class to calculetar to sum of to two numbers
+ class  calculator {
+     int add(int n1,int n2){
+        return (n1+n2);
+     }
+
+     
+    
+}
+public class oop_consept {
+    public static void main(String[] args) {
+
+        int num1=10;
+        int num2=20;
+        calculator cal=new calculator();
+        int result=cal.add(num1,num2);
+        System.out.println("sum:"+result);
+    }
+}
