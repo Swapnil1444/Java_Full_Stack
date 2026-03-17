359f1e825501c72382b02bcec508519de514a523
Swapnil Supekar <swpnilsupekar@gmail.com>
2026-03-17 23:48:12 +0530

    new

--- Java_Learn/src/oop/Interface/functionInterface_alotestion.java
+++ Java_Learn/src/oop/Interface/functionInterface_alotestion.java
@@ -1,16 +1,16 @@
 package oop.Interface;
 
 @FunctionalInterface
-interface A{
+interface Abc{
     void dispaly();
      // void a(); functional interface not a create multipal method in singale intrface
-     
+
 }
 
 public class functionInterface_alotestion {
     public static void main(String argd[]){
 
-        A obj=()-> System.out.println("display method");
+        Abc obj=()-> System.out.println("display method");
 
         obj.dispaly();
     }
